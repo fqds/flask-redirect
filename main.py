dcbe15ac94887f65d9ebf9731dcdbf6e7aec65ea
fqds <121273hvv@gmail.com>
2023-11-14 15:21:47 +0300

    ssl

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ def hello(_):
     return redirect(config.TO_URL, code=302)
 
 if __name__ == '__main__':
-    if config.WITH_CERT: context = (config.CERT_PATH+'cert.pem', config.CERT_PATH+'key.pem')
+    if config.WITH_CERT: context = (config.CERT_PATH+'fullchain.pem', config.CERT_PATH+'privkey.pem')
     else: context = None
     app.run(host=config.FROM_HOST, port=config.FROM_PORT, ssl_context=context)
